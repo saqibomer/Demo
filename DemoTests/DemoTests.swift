1b98cf5fecc83d02604d9beca0a17d333f94ebb8
Saqib <saqibomer.cs@gmail.com>
2021-04-11 17:14:49 +0500

    - Added unit testing

--- DemoTests/DemoTests.swift
+++ DemoTests/DemoTests.swift
@@ -8,19 +8,30 @@
 import XCTest
 @testable import Demo
 
+
 class DemoTests: XCTestCase {
+    
+    let viewModel = GamesViewModel(networkingService: NetworkClient() as NetworkingService)
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        viewModel.didFetchGames = { [weak self] in
+            guard let strongSelf = self else { return }
+            
+            if strongSelf.viewModel.games.count > 0 {
+//                XCTAssertTrue()
+            }
+        }
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGamesFetch() throws {
+        
+        viewModel.startFetchingGames()
+        
     }
 
     func testPerformanceExample() throws {
@@ -28,6 +39,14 @@ class DemoTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testDecoding() throws {
+        /// When the Data initializer is throwing an error, the test will fail.
+        let jsonData = try Data(contentsOf: URL(string: "https://api.rawg.io/api/games?page_size=10&page=1")!)
+
+        /// The `XCTAssertNoThrow` can be used to get extra context about the throw
+        XCTAssertNoThrow(try JSONDecoder().decode(GamesResponse.self, from: jsonData))
     }
 
 }
